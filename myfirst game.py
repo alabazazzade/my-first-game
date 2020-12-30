5a15c5a4c3347462cb7492763af980cc442e381a
Ala Bazazzadeh <73437440+alabazazzade@users.noreply.github.com>
2020-12-30 22:32:12 +0330

    Add files via upload

--- myfirst game.py
+++ myfirst game.py
@@ -5,72 +5,82 @@ bg = pygame.image.load('bg.jpg')
 char = pygame.image.load('standing.png')
 pygame.init()
 win = pygame.display.set_mode((500,480))
-pygame.display.set_caption('First Game')
-x = 50
-y = 400
-width = 40
-hight = 60
-vel = 7
-isjump = False
-jumpcnt = 10
-run = True
-right = False
-left = False
-walkcount = 0
-clk = pygame.time.Clock()
+pygame.display.set_caption('This man walks!')
+
+class Person(object):
+    def __init__(self, x , y ,width , hight , vel):
+        self.x = x
+        self.y = y
+        self.vel = vel
+        self.width = width
+        self.hight = hight
+        self.isjump = False
+        self.jumpcnt = 10
+        self.run = True
+        self.right = False
+        self.left = False
+        self.walkcount = 0
+
+        
+    def drow(self,win): 
+        win.blit(bg , (0,0))
+        if self.walkcount + 1 > 27 :
+            self.walkcount = 0
+            win.blit(walkRight[self.walkcount//3],(self.x,self.y))
+        if man.left:
+            win.blit(walkLeft[self.walkcount//3] , (self.x,self.y))
+            self.walkcount += 1 
+        elif man.right :
+            win.blit(walkRight[self.walkcount//3] , (self.x,self.y))
+            self.walkcount += 1
+    
+
 def redrowgamewindow():
-    global walkcount
     win.blit(bg , (0,0))
-    if walkcount + 1 > 27 :
-        walkcount = 0
-        win.blit(walkRight[walkcount//3],(x,y))
-    if left:
-        win.blit(walkLeft[walkcount//3] , (x,y))
-        walkcount +=1
-    elif right :
-        win.blit(walkRight[walkcount//3] , (x,y))
-        walkcount +=1
+    man.drow(win)
     pygame.display.update()
 
-while run:
-    
+        
+man = Person(300,410,64,64,5)
+clk = pygame.time.Clock()
+
+while man.run: 
     clk.tick(27)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            man.run = False
     keys = pygame.key.get_pressed()
     
-    if keys[pygame.K_LEFT] and x > vel-1:
-        x -= vel
-        left = True
-        right = False
-    elif keys[pygame.K_RIGHT] and x<500-vel-width+1:
-        x += vel
-        left = False
-        right = True
+    if keys[pygame.K_LEFT] and man.x  > man.vel - 1:
+        man.x -= man.vel
+        man.left = True
+        man.right = False
+    elif keys[pygame.K_RIGHT] and man.x < 500 - man.vel - man.width+1:
+        man.x += man.vel
+        man.left = False
+        man.right = True
     else :
-        left = False
-        right = False
-        walkcount = 0
-    if not isjump:
-        
+        man.left = False
+        man.right = False
+        man.walkcount = 0
+    if not man.isjump: 
         if  keys[pygame.K_SPACE]:
-            isjump = True
-            left = False
-            right = False
-            walkcount = 0
+            man.isjump = True
+            man.left = False
+            man.right = False
+            man.walkcount = 0
     else:
-        if jumpcnt >= -10:
+        if man.jumpcnt >= -10:
             neg = 1
-            if jumpcnt < 0 :
+            if man.jumpcnt < 0 :
                  neg = -1 
                 
-            y -= (jumpcnt ** 2)*0.5 * neg
-            jumpcnt -= 1
+            man.y -= (man.jumpcnt ** 2)*0.5 * neg
+            man.jumpcnt -= 1
         else:
-            isjump = False
-            jumpcnt = 10
+            man.isjump = False
+            man.jumpcnt = 10
       
     redrowgamewindow()
 pygame.quit()
